675291ac430bac0f23d4c6f4fcf12fb2eebfa300
James Yarbrough <jjarbrough@gmail.com>
2023-10-10 14:44:24 -0500

    made the temperature converters

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(75));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(100));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (5.0 / 9.0) * (fahrenheit - 32);
+            return celsius;
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * (9.0 / 5.0)) + 32;
+            return fahrenheit;
+        }
+    }
+}
